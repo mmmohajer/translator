28b74dc051f8557285fe59a9ab47a1be68a6fdff
mmmohajer70 <mmmohajer70@gmail.com>
2025-08-04 15:43:08 -0400

    Automation system improved

--- api/config/settings/celery.py
+++ api/config/settings/celery.py
@@ -10,11 +10,3 @@ CELERY_BROKER_URL = f"redis://:{REDIS_USER_PASS}@redis:6379/1"
 CELERY_TIMEZONE = os.environ.get('API_TIME_ZONE', 'America/Toronto')
 
 CELERY_BEAT_SCHEDULE = {}
-
-# if not DEBUG:
-#     CELERY_BEAT_SCHEDULE = {
-#         'sync_all_app_courses_periodically_task': {
-#             'task': 'automation.tasks.sync_all_app_courses_periodically_task',
-#             'schedule': crontab(minute=30, hour='0,12')
-#         }
-#     }
